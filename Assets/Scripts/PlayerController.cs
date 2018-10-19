108f148ee36e27a95fa7cc693c381f50cefe2f8f
Machan_Brandon <machb608@hsd.k12.or.us>
2018-10-19 13:21:16 -0700

    added basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    private Rigidbody m_rb;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +14,14 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		
 	}
+    private void FixedUpdate()
+    {
+        /* get user input to apply a horizontal
+         * force to our player object */
+
+        float movement = Input.GetAxis("Horizontal");
+        //add force
+        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+
+    }
 }
