2a5b6eef54db080c62389b8a198f1bb5a9fe7136
Machan_Brandon <machb608@hsd.k12.or.us>
2018-10-23 13:01:08 -0700

    added player movement and background

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody m_rb;
-
+    public float speed = 10.0F;
+    public float max_speed = 12.0F;
 	// Use this for initialization
 	void Start () {
-		
+        m_rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -21,7 +22,8 @@ public class PlayerController : MonoBehaviour {
 
         float movement = Input.GetAxis("Horizontal");
         //add force
-        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+        m_rb.AddForce(new Vector3(movement * speed, 0.0F, 0.0F));
+        m_rb.velocity = new Vector3(Mathf.Clamp(m_rb.velocity.x, -max_speed, max_speed),m_rb.velocity.y,m_rb.velocity.z); 
 
     }
 }
