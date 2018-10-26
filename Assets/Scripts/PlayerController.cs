62cc156715e5b3917b6ff9542f57d2fbe59ca513
Machan_Brandon <machb608@hsd.k12.or.us>
2018-10-26 13:05:53 -0700

    added jumping mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody m_rb;
-    public float speed = 10.0F;
-    public float max_speed = 12.0F;
+    public float speed = 10.0F ;
+    public float max_speed = 15.0F ;
+    public float jump_height = 200F ;
 	// Use this for initialization
 	void Start () {
         m_rb = GetComponent<Rigidbody>();
@@ -23,7 +24,12 @@ public class PlayerController : MonoBehaviour {
         float movement = Input.GetAxis("Horizontal");
         //add force
         m_rb.AddForce(new Vector3(movement * speed, 0.0F, 0.0F));
-        m_rb.velocity = new Vector3(Mathf.Clamp(m_rb.velocity.x, -max_speed, max_speed),m_rb.velocity.y,m_rb.velocity.z); 
+        m_rb.velocity = new Vector3(Mathf.Clamp(m_rb.velocity.x, -max_speed, max_speed),m_rb.velocity.y,m_rb.velocity.z);
 
+        //jumping
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_rb.AddForce(0.0F, jump_height, 0.0F);
+        }
     }
 }
